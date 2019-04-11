49ba6afcca29b95971dccff3c34caf6fd3709845
Daniel Linsley <dlinsley@vmware.com>
2019-04-10 23:19:09 -0700

    be specific about the vco.version during concourse build

--- ci/build-packages/build.sh
+++ ci/build-packages/build.sh
@@ -5,8 +5,8 @@
 for POM in $(ls */pom.xml)
 do
   echo "Building $POM"
-  mvn -f $POM -s /usr/share/maven/ref/settings-docker.xml -o versions:set -DnewVersion=$(cat o11n-package-vroremote-version/version)
-  mvn -f $POM -s /usr/share/maven/ref/settings-docker.xml -o package -PconcourseDistOut
+  mvn -f $POM -s /usr/share/maven/ref/settings-docker.xml -o versions:set -Dvco.version=$VCO_VERSION -DnewVersion=$(cat o11n-package-vroremote-version/version)
+  mvn -f $POM -s /usr/share/maven/ref/settings-docker.xml -o package -Dvco.version=$VCO_VERSION -PconcourseDistOut
   if [ $? -eq 0 ]; then
     echo -e "=== Successfully built package $POM ==="
   else
